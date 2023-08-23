589e11094948bf02cc7042a73dca653f3d105d0b
talal-af <142681437+talal-af@users.noreply.github.com>
2023-08-23 20:02:02 +0300

    talal

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Change the following print statement so that it uses the addition operator + and prints out 8
-
+print("talal")
 # Write a print statement that uses the division operator / 
-
+print (4+4)
 # Add a print statement that uses multiplication *
-
+print(6/2)
 # Create a variable called `length` and assign a value to it
-
+print(2*2)
 # Create a variable called    `width` and assign a value to it
 
 # Create a variable called `area` and assign to it the multiplication of `length` and `width`
@@ -13,4 +13,9 @@
 # Print `area` in the following: "The result is " followed by the `area` variable
 
 
-# BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+# BONUS: Print the area in the following syntax: "  lenIfgth of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+length=15 
+width=20
+area=length*width
+print(f"the result is{area}")
+print(f"if the length of rectangle is {length},and width is {width} then the area is:{area}")
